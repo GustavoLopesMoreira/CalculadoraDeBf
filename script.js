62e5e47547922a70ac402333153aae7b635a46df
Gustavo Lopes Moreira <GustavoLMConta@Gmail.Com>
2025-09-06 12:40:07 -0300

    feat: implementar exibição do formulário com JavaScript e atualizar cores dos títulos
    - Criada função em JavaScript para exibir/esconder formulário ao clicar no botão
    -Ajustada coloração dos títulos para melhor aparencia no site

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function aparecerFormulario(){
-    document.getElementById("teste").innerHTML = "teste";
+function  aparecertextoh() {
+    document.getElementById("formularioh").style.display = "block";
+    document.getElementById("formulariom").style.display = "none"  
 }
+ function aparecertextom() {
+   document.getElementById("formulariom").style.display = "block";
+   document.getElementById("formularioh").style.display = "none";
+ }
