bd08a30414ad3eda8e4b80186685f15969265084
Gustavo Lopes Moreira <GustavoLMConta@Gmail.Com>
2025-09-09 13:38:03 -0300

    feat: implementa função para calcular  dados corporais em JavaScript
    - Criada função em JavaScript para calcular e exibir dados corporais masculinos
    
    - Ajustada pequenas mudanças visuais para melhor acabamento do site

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function  aparecertextoh() {
-    document.getElementById("formularioh").style.display = "block";
-    document.getElementById("formulariom").style.display = "none"  
+function aparecertextoh() {
+  document.getElementById("formularioh").style.display = "block";
+  document.getElementById("formulariom").style.display = "none"
 }
- function aparecertextom() {
-   document.getElementById("formulariom").style.display = "block";
-   document.getElementById("formularioh").style.display = "none";
- }
+function aparecertextom() {
+  document.getElementById("formulariom").style.display = "block";
+  document.getElementById("formularioh").style.display = "none";
+}
+
+function calcularBfM() {
+  let altura = parseFloat(document.getElementById("alturah").value);
+  let alturaIn = altura / 2.54;
+  let cintura = parseFloat(document.getElementById("cinturah").value);
+  let cinturaIn = cintura / 2.54;
+  let pescoco = parseFloat(document.getElementById("pescocoh").value);
+  let pescocoIn = pescoco / 2.54;
+  let peso = parseFloat(document.getElementById("pesoh").value);
+
+  let formulabf = 86.010 * Math.log10(cinturaIn - pescocoIn) - 70.041 * Math.log10(alturaIn) + 36.76;
+  let formulaMassaGorda = ((formulabf / 100) * peso);
+  let formulaMassaMagra = peso - formulaMassaGorda;
+    document.getElementById("bf").innerHTML = formulabf.toFixed(2) + "%";
+  document.getElementById("mm").innerHTML = formulaMassaMagra.toFixed(2) + "KG"
+  document.getElementById("mg").innerHTML = formulaMassaGorda.toFixed(2) + "KG"
+}
